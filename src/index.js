b729958b5649c392d077c5c88f20ea1591e64d97
GKephart <gkephart@cnm.edu>
2020-05-22 14:52:16 -0600

    added redux toolkit configure store

--- src/index.js
+++ src/index.js
@@ -3,35 +3,33 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css'
-import thunk from "redux-thunk";
-import {applyMiddleware, createStore} from "redux";
-import {reducers} from "./shared/reducers";
-import {Provider} from "react-redux";
-import {Route, BrowserRouter, Switch} from "react-router-dom";
-import {FourOhFour} from "./pages/four-oh-four/FourOhFour";
-import {Home} from "./pages/home/Home";
-import {UserPosts} from "./pages/userPosts/UserPosts";
-import {Posts} from "./pages/posts/Posts";
+import { reducers } from "./shared/reducers";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { FourOhFour } from "./pages/four-oh-four/FourOhFour";
+import { Home } from "./pages/home/Home";
+import { UserPosts } from "./pages/userPosts/UserPosts";
+import { Posts } from "./pages/posts/Posts";
+import { configureStore } from '@reduxjs/toolkit'
 
 //In order to use redux a store must be initialized and passed to the Provider component.
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = configureStore({reducer: reducers});
 
 // Routing allows for react to have routed pages.
 const Routing = (store) => (
-	<>
+  <>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/user/:userId" component={UserPosts} userId=":userId"/>
+          <Route exact path="/posts" component={Posts}/>
+          <Route exact path="/" component={Home}/>
+          <Route component={FourOhFour}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
 
-	<Provider store={store}>
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/user/:userId" component={UserPosts} userId=":userId"/>
-				<Route exact path="/posts" component={Posts} />
-				<Route exact path="/" component={Home}/>
-				<Route component={FourOhFour}/>
-			</Switch>
-		</BrowserRouter>
-	</Provider>
-
-	</>
+  </>
 );
 
 ReactDOM.render(Routing(store), document.querySelector("#root"));
